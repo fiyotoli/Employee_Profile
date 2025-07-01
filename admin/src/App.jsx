ed31f0b5e7c3b4b1c38974fac04955c196f94743
fiyotoli <tasfayneshtolasa35@gmail.com>
2025-07-01 03:38:02 +0300

    some update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -14,6 +14,7 @@ import SingleBlog from './pages/SingleBlog';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AdminFeedbackList from './pages/AdminFeedbackList';
+import Login from './components/Login';
 
 
 
